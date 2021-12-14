06eeb72b7b93d6c866b5ab7d7db0827425bb0ba3
Aryan Singh <aryan.singh@ul.ie>
2021-12-14 19:06:05 +0000

    rfile

--- network_plots_while_learning.R
+++ network_plots_while_learning.R
@@ -5,3 +5,16 @@ library(sna)
 drugpaj <- read.paj('data_networks/drugnet2.paj')
 drug <- drugpaj$networks[[1]]
 gender <- drugpaj$partitions[[1]] 
+
+suppressPackageStartupMessages(library(knitr)) 
+kable(table(gender), col.names=c("Gender","Frequency"))
+
+ethnicity <- drugpaj$partitions[[2]]
+kable(table(ethnicity), col.names=c("Ethnicity","Frequency"))
+
+png(filename="plots/simple_drug_user_plots.png")
+plot(drug)
+dev.off()
+
+
+#plot(drug, vertex.col = colors, vertex.side = sides ,)
