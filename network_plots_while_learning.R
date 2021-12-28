1865579c0c16068c45c00dfb15f4bdcde4cf336b
Aryan Singh <aryan.singh@ul.ie>
2021-12-28 13:41:53 +0000

    week2 10%

--- network_plots_while_learning.R
+++ network_plots_while_learning.R
@@ -1,7 +1,5 @@
 library(sna)
 
-
-
 drugpaj <- read.paj('data_networks/drugnet2.paj')
 drug <- drugpaj$networks[[1]]
 gender <- drugpaj$partitions[[1]] 
@@ -16,5 +14,89 @@ png(filename="plots/simple_drug_user_plots.png")
 plot(drug)
 dev.off()
 
+#story of high turn-over case study
 
-#plot(drug, vertex.col = colors, vertex.side = sides ,)
+# sociomatrix data for a network
+
+num_nodes <- 15
+
+my_matrix <- matrix(round(runif(num_nodes*num_nodes)),
+                    nrow = num_nodes,
+                    ncol = num_nodes)
+
+my_matrix
+
+diag(my_matrix) <- 0
+dim(my_matrix)
+
+class(my_matrix)
+
+sum(is.na(my_matrix))
+
+
+my_network <- as.network(x = my_matrix,
+                         directed = TRUE,
+                         loop = FALSE,
+                         matrix.type = "adjacency")
+
+plot(my_network)
+
+par(mar=c(0,0,1,0))# bottom , left , top , right , adjust margin all to 0 except top
+plot(my_network)
+
+plot(my_network,main = "Random network")
+
+library(foreign)
+# read .dta // stata file
+# read.xport // sax xport
+
+drugpaj <- read.paj("data_networks/drugnet2.paj")
+names(drugpaj)
+names(drugpaj$networks)
+names(drugpaj$partitions)
+
+drug <- drugpaj$networks[[1]]
+class(drug)
+plot(drug)
+
+
+gender <- drugpaj$partitions[[1]]
+table(gender)
+
+
+female <- ifelse(gender == 2,1,
+                 ifelse(gender == 1,0,NA))
+
+drug <- set.vertex.attribute(drug,'female',value = c(female))
+
+ethnicity <- drugpaj$partitions[[2]] 
+table(ethnicity)
+
+
+drug <- set.vertex.attribute(drug,'ethnicity',value = c(ethnicity))
+
+colors <- ifelse(gender == 2,"palevioletred",ifelse(gender == 1,"royalblue2","gray8"))
+
+
+par(mar=c(0,0,0,0))
+plot(drug,vertex.col = colors)
+
+
+load("data_networks/flo.Rdata")
+
+flo.marriage <- as.network(as.matrix(flo.marriage),directed = FALSE)
+flo.biz <- as.network(as.matrix(flo.biz),directed = FALSE)
+
+
+set.vertex.attribute(flo.marriage,'wealth',flo.att[,2])
+set.vertex.attribute(flo.biz,'wealth',flo.att[,2])
+
+par(mar=c(1,1,1,1), mfrow = c(1,2))
+
+plot(flo.marriage,vertex.cex =(get.vertex.attribute(flo.marriage,'wealth')/25),
+      displaylabels = TRUE)
+
+plot(flo.biz,vertex.cex =(get.vertex.attribute(flo.biz,'wealth')/25),
+     displaylabels = TRUE)
+
+
